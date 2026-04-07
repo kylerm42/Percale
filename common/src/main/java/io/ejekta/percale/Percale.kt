6d83a25a8e4876b6ca28dad0bf5d5ecd816d507c
Kyle Moore <kylerm42@users.noreply.github.com>
2026-04-07 15:29:35 -0400

    Disable verbose serialization debug logging by default

--- common/src/main/java/io/ejekta/percale/Percale.kt
+++ common/src/main/java/io/ejekta/percale/Percale.kt
@@ -7,7 +7,7 @@ import java.util.logging.Formatter
 
 
 object Percale {
-    var shouldSyslog = true
+    var shouldSyslog = false
 
     val LOGGER = Logger.getLogger("Percale").apply {
         useParentHandlers = false
